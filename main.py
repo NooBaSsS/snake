db0a9eff9d907db0cbb738985700ad38cca5bae2
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2024-04-06 14:44:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import tkinter
 
 WINDOW_BG = 'black'
 CANVAS_BG = 'white'
-TILE_SIZE = 16
+TILE_SIZE = 32
 LINE_COLOR = 'black'
+SNAKE_COLOR = 'red'
 FPS = 30
 
 
@@ -26,11 +27,14 @@ class App:
         )
         self.canvas.pack()
         self.draw_lines()
+        self.game = Game(self.canvas, self.tiles_ammount)
         self.window.mainloop()
 
     def on_key(self, event: tkinter.Event) -> None:
         if event.keysym == 'Escape':
             self.window.destroy()
+        else:
+            self.game.on_key(event)
 
     def draw_lines(self) -> None:
         for i in range(1, int(self.tiles_ammount)):
@@ -51,13 +55,82 @@ class App:
 
 
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, canvas: tkinter.Canvas, tiles_ammount: int) -> None:
+        self.canvas = canvas
+        self.size = tiles_ammount
+        self.snake = Snake(
+            TILE_SIZE * (self.size // 2),
+            TILE_SIZE * (self.size // 2),
+            TILE_SIZE,
+            canvas,
+            SNAKE_COLOR,
+            'Up',
+            'Down',
+            'Left',
+            'Right',
+        )
+        self.update()
+
+    def update(self) -> None:
+        self.canvas.delete('snake')
+        self.snake.move()
+        self.snake.draw()
+        self.canvas.after(1000 // FPS, self.update)
+
+    def on_key(self, event) -> None:
+        self.snake.on_key(event)
 
 
 class Snake:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+            self,
+            col: int,
+            row: int,
+            size: int,
+            canvas: tkinter.Canvas,
+            color: str,
+            key_up: str,
+            key_down: str,
+            key_left: str,
+            key_right: str,
+    ) -> None:
+        self.col = col
+        self.row = row
+        self.size = size
+        self.color = color
+        self.canvas = canvas
+        self.key_up = key_up
+        self.key_down = key_down
+        self.key_left = key_left
+        self.key_right = key_right
+        self.direction = (1, 0)
+
+    def draw(self) -> None:
+        self.canvas.create_rectangle(
+            self.col,
+            self.row,
+            self.col + self.size,
+            self.row + self.size,
+            fill=self.color,
+            tags='snake'
+        )
+
+    def on_key(self, event: tkinter.Event) -> None:
+        if event.keysym == self.key_right:
+            self.change_direction((1, 0))
+        if event.keysym == self.key_left:
+            self.change_direction((-1, 0))
+        if event.keysym == self.key_up:
+            self.change_direction((0, -1))
+        if event.keysym == self.key_down:
+            self.change_direction((0, 1))
+
+    def change_direction(self, direction) -> None:
+        self.direction = direction
+
+    def move(self) -> None:
+        self.col += self.size * self.direction[0]
+        self.row += self.size * self.direction[1]
 
 
 class Food:
